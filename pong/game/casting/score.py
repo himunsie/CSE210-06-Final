fe61e0bcd22fad6e3fcb20a219e36cbd03f75708
Heidi <hlmunson@hotmail.com>
2022-03-25 21:39:46 -0600

    update score

--- pong/game/casting/score.py
+++ pong/game/casting/score.py
@@ -1,5 +1,6 @@
 from game.casting.actor import Actor
-
+from game.shared.color import Color
+from game.shared.point import Point
 
 class Score(Actor):
     """
@@ -12,16 +13,22 @@ class Score(Actor):
     Attributes:
         _points (int): The points earned in the game.
     """
-    def __init__(self):
-        super().__init__()
-        self._points = 0
-        self.add_points(0)
+    def __init__(self, position):
+        self._text = ""
+        self._font_size = 15
+        self._color = Color(255, 255, 255)
+        self._points = -1
+        self.add_points()
+        self._position = position
+        self._velocity = Point(0, 0)
 
-    def add_points(self, points):
+    def add_points(self):
         """Adds the given points to the score's total points.
         
         Args:
             points (int): The points to add.
         """
-        self._points += points
+        self._points += 1
         self.set_text(f"{self._points}")
+    def total_points(self):
+        return self._points

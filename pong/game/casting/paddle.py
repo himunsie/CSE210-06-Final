b37543b2e206a2dd64204e5407ef4fce333726e7
Heidi <hlmunson@hotmail.com>
2022-03-25 22:07:52 -0600

    updating paddle

--- pong/game/casting/paddle.py
+++ pong/game/casting/paddle.py
@@ -6,15 +6,18 @@ from game.casting.point import Point
 class Paddle(Actor):
     """A implement used to hit and bounce the ball in the game."""
     
-    def __init__(self, body):
+    def __init__(self,position, image):
         """Constructs a new Paddle.
         
         Args:Args:
-            body: A new instance of Body.
+            position: A new instance of Paddle.
+            image: a new instance of image
             
         """
         super().__init__()
-        self._body = body
+        # self._body = body
+        self._position = position
+        self._image = image
         
     def get_body(self):
         """Gets the paddle's body.
@@ -23,27 +26,34 @@ class Paddle(Actor):
             An instance of Body.
         """
         return self._body
+    def get_image(self):
+        """Gets the paddle's image.
+        
+        Returns:
+            An instance of Image.
+        """
+        return self._image
 
     def move_next(self):
         """Moves the paddle using its velocity."""
-        position = self._body.get_position()
-        velocity = self._body.get_velocity()
+        position = self.get_position()
+        velocity = self.get_velocity()
         new_position = position.add(velocity)
-        self._body.set_position(new_position)
+        self.set_position(new_position)
 
     def move_up(self):
         """Moves the paddle up"""
         
         velocity = Point(-PADDLE_VELOCITY, 0)
-        self._body.set_velocity(velocity)
+        self.set_velocity(velocity)
         
     def move_down(self):
         """Moves the paddle down"""
         
         velocity = Point(PADDLE_VELOCITY, 0)
-        self._body.set_velocity(velocity)
+        self.set_velocity(velocity)
     
     def stop_moving(self):
         """Stops the paddle from moving."""
         velocity = Point(0, 0)
-        self._body.set_velocity(velocity)
+        self.set_velocity(velocity)

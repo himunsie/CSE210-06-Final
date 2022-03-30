d38b321ff3651328cbaf0f7b63d1ca98d048db04
Heidi <hlmunson@hotmail.com>
2022-03-29 19:50:08 -0600

    paddles moving, wrong direction

--- pong/game/scripting/move_actors_action.py
+++ pong/game/scripting/move_actors_action.py
@@ -1,3 +1,5 @@
+import constants 
+from game.casting.point import Point
 from game.scripting.action import Action
 
 class MoveActorsAction(Action):
@@ -17,3 +19,33 @@ class MoveActorsAction(Action):
         velocity = ball.get_velocity()
         position = position.add(velocity)
         ball.set_position(position)
+
+        paddle1 = cast.get_first_actor("paddle1")
+
+        velocity = paddle1.get_velocity()
+        position = paddle1.get_position()
+        x = position.get_x()
+        
+        position = position.add(velocity)
+
+        if x < 0:
+            position = Point(0, position.get_y())
+        elif x > (1040 - 28):
+            position = Point(1040 - 28, position.get_y())
+            
+        paddle1.set_position(position)
+
+        paddle2 = cast.get_first_actor("paddle2")
+        
+        velocity = paddle2.get_velocity()
+        position = paddle2.get_position()
+        x = position.get_x()
+        
+        position = position.add(velocity)
+
+        if x < 0:
+            position = Point(0, position.get_y())
+        elif x > (1040 - 28):
+            position = Point(1040 - 28, position.get_y())
+            
+        paddle2.set_position(position)

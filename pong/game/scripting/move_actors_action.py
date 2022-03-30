cd3d8d7a2cd38f8af5d2ceb564be2cb9376bb654
Heidi <hlmunson@hotmail.com>
2022-03-29 19:25:55 -0600

    ball is moving

--- pong/game/scripting/move_actors_action.py
+++ pong/game/scripting/move_actors_action.py
@@ -2,8 +2,9 @@ from game.scripting.action import Action
 
 class MoveActorsAction(Action):
 # Implement MoveActorsAction class here! 
-
-    # Override the execute(cast, script) method as follows:
+    def __init__(self):
+        pass
+    
     def execute(self, cast, script):
         """Executes the move actors action.
 
@@ -11,12 +12,8 @@ class MoveActorsAction(Action):
         cast (Cast): The cast of Actors in the game.
         script (Script): The script of Actions in the game.
         """
-        actors = cast.get_all_actors()
-        for actor in actors:
-            actor.move_next()
-# 1) get all the actors from the cast
-        # actors = cast.get_all_actors()
-# 2) loop through the actors
-        # for actor in actors:
-# 3) call the move_next() method on each actor
-                # actor.move_next()
+        ball = cast.get_first_actor("ball")
+        position = ball.get_position()
+        velocity = ball.get_velocity()
+        position = position.add(velocity)
+        ball.set_position(position)
